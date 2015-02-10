c4367590c054a33d46f61ac39c318445dfbdd71f
sylvesterlan100 <sylvesterlan100@gmail.com>
2015-02-10 11:33:48 -0800

    pseudo added

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 var attr=['name', 'type', 'categories', 'abilities', 'weaknesses']
 
+//Creating angular app
 var app = angular.module('myApp', ['firebase']);
 
+//Creating angular app   !!! The array looking stuff here is not necessary
+//app.controller('MainController', function($scope, $firebase){.....})     is fine
 app.controller('MainController', ['$scope', '$firebase', function($scope, $firebase){
+	
+	//Linking my app to the firebase account here
 	var ref = new Firebase('https://sylvesterpoke.firebaseio.com/')
 	var sync = $firebase(ref);
 
-	//Firebase Read-only Object
-	//$scope.data = sync.$asObject();
-
-	//Firebase 3-way Binding Object
-	//var syncObject = sync.$asObject();
-	//syncObject.$bindTo($scope, 'data');
-
-	function Pokemon(name, type, cateogries, abilities, weaknesses){
-		this.name=name;
-		this.type=type;
-		this.categories=categories;
-		this.abilities=abilities;
-		this.weaknesses=weaknesses;
-	}
-
-	//Firebase Array
+	//The firebase is synced to my locally created pokemonList
+	//Changes to pokemon list
 	$scope.pokemonList=sync.$asArray();
+
+	
+	//Function for adding pokemon to my firebase database
 	$scope.addPoke=function(){
+
+		//$add syntax is unique to angularfirebase
 		$scope.pokemonList.$add($scope.pokemon);
 		$scope.pokemon={};
 	};
 
+	//Function for deleting pokemon to my firebase database
 	$scope.deletePoke=function(released){
+		//$remove is syntax unique to angularfire
 		$scope.pokemonList.$remove(released);
 	};
 
+	//Function for updating pokemon in my firebase
 	$scope.updatePoke=function(updated){
-		updated.$save();
+		//$remove is syntax unique to angularfire
+		//Simply update the list with the passed pokemon
+		$scope.pokemonList.$save(updated);
 	};
 
 }]);
